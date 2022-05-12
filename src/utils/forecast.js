e35c67ae41d91d06c091262b913774fad2b84834
Grant McDonnal <grantmcdonnal@gmail.com>
2022-05-11 20:41:02 -0700

    update

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,19 +1,34 @@
-const request = require('request')
+const request = require("request");
 
 const forecast = (long, lat, callback) => {
-    const url = 'https://api.darksky.net/forecast/b880ec89a459b4c0aebfabfe76028434/' + lat + ',' + long
+  const url =
+    "https://api.darksky.net/forecast/b880ec89a459b4c0aebfabfe76028434/" +
+    lat +
+    "," +
+    long;
 
-    request({url: url, json: true}, (error, {body}) => {
-        //console.log(body.daily.data)
-        if(error){
-            callback("Unable to connect to weather service", undefined)
-        }else if (body.error){
-            callback("Unable to find location", undefined)
-        }else{
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out. The high today is " + 
-            body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + "% chance of rain.")
-        }
-    })
-}
+  request({ url: url, json: true }, (error, { body }) => {
+    console.log(body.daily.data);
+    if (error) {
+      callback("Unable to connect to weather service", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degrees out. The high today is " +
+          body.daily.data[0].temperatureHigh +
+          " with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
